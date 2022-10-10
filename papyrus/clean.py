3ef332396f1e3528dff3338740977aeede49308a
Emrah Diril <emrah@diril.org>
2022-10-09 22:11:37 -0400

    Clean post store too

--- papyrus/clean.py
+++ papyrus/clean.py
@@ -8,3 +8,5 @@ def clean(args, conf):
     if site_dir.exists():
         shutil.rmtree(site_dir)
     site_dir.mkdir()
+    post_store_path = pathlib.Path('.') / conf['POST_STORE']
+    post_store_path.unlink(missing_ok=True)
